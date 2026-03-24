6cf5661d66f765f4af4e9d09c7c1042a899ccae3
aryan-coder-777 <aryanshuklacoc@gmail.com>
2026-03-24 09:36:46 +0530

    UC4: Maintain ordered bogie consist using LinkedList

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.LinkedList;
 
 public class TrainConsistApp {
 
@@ -59,12 +60,42 @@ public class TrainConsistApp {
         bogieIds.add("BG101");
         bogieIds.add("BG102");
         bogieIds.add("BG103");
-        bogieIds.add("BG101"); // duplicate
-        bogieIds.add("BG102"); // duplicate
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
 
         System.out.println("\nBogie IDs in the train:");
         System.out.println(bogieIds);
 
         System.out.println("\nNote: Duplicate IDs are automatically removed.");
+
+
+        // ================= UC4 =================
+        System.out.println("\n==========================================");
+        System.out.println(" UC4 - Maintain Ordered Bogie Consist ");
+        System.out.println("==========================================");
+
+        LinkedList<String> trainOrder = new LinkedList<>();
+
+        trainOrder.add("Engine");
+        trainOrder.add("Sleeper");
+        trainOrder.add("AC");
+        trainOrder.add("Cargo");
+        trainOrder.add("Guard");
+
+        System.out.println("\nInitial Train Consist:");
+        System.out.println(trainOrder);
+
+        trainOrder.add(2, "Pantry Car");
+
+        System.out.println("\nAfter Inserting 'Pantry Car' at position 2:");
+        System.out.println(trainOrder);
+
+        trainOrder.removeFirst();
+        trainOrder.removeLast();
+
+        System.out.println("\nAfter Removing First and Last Bogie:");
+        System.out.println(trainOrder);
+
+        System.out.println("\nUC4 ordered consist operations completed.");
     }
 }
